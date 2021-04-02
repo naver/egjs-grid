4f98fc0c7ad5139e05d8d3b003fdc88655480fb1
Daybrush <daybrush@gmail.com>
2021-04-02 13:32:56 +0900

    skip: apply review

--- src/grids/PackingGrid.ts
+++ src/grids/PackingGrid.ts
@@ -57,8 +57,14 @@ export interface PackingGridOptions extends GridOptions {
 }
 
 /**
- * The PackingGrid is a grid that shows the important items bigger without sacrificing the weight of the items. Rows and columns are separated so that items are dynamically placed within the horizontal and vertical space rather than arranged in an orderly fashion. The position and size of the items are different depending on the values of the `sizeWeight` and `ratioWeight` options.
- * @ko PackingGrid는 아이템의 본래 크기에 따른 비중을 해치지 않으면서 중요한 카드는 더 크게 보여 주는 레이아웃이다. 행과 열이 구분돼 아이템을 정돈되게 배치하는 대신 가로세로 일정 공간 내에서 동적으로 아이템을 배치한다. `sizeWeight`와 `ratioWeight` 옵션 값에 따라 아이템이 배치되는 위치, 사이즈가 달라진다.
+ * The PackingGrid is a grid that shows the important items bigger without sacrificing the weight of the items.
+ * Rows and columns are separated so that items are dynamically placed within the horizontal and vertical space rather than arranged in an orderly fashion.
+ * If `sizeWeight` is higher than `ratioWeight`, the size of items is preserved as much as possible.
+ * Conversely, if `ratioWeight` is higher than `sizeWeight`, the ratio of items is preserved as much as possible.
+ * @ko PackingGrid는 아이템의 본래 크기에 따른 비중을 해치지 않으면서 중요한 카드는 더 크게 보여 주는 레이아웃이다.
+ * 행과 열이 구분돼 아이템을 정돈되게 배치하는 대신 가로세로 일정 공간 내에서 동적으로 아이템을 배치한다.
+ * `sizeWeight`가 `ratioWeight`보다 높으면 아이템들의 size가 최대한 보존이 된다.
+ * 반대로 `ratioWeight`가 `sizeWeight`보다 높으면 아이템들의 비율이 최대한 보존이 된다.
  * @memberof Grid
  * @param {HTMLElement | string} container - A base element for a module <ko>모듈을 적용할 기준 엘리먼트</ko>
  * @param {Grid.PackingGrid.PackingGridOptions} options - The option object of the PackingGrid module <ko>PackingGrid 모듈의 옵션 객체</ko>
