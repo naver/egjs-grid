b7ed9ed837d531931c5ae9ed8259fca9f64dbf72
Daybrush <daybrush@gmail.com>
2021-04-30 19:09:44 +0900

    chore: fix karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,63 +1,56 @@
-module.exports = function(config) {
-	const videoProxies = {};
+module.exports = function (config) {
+  const karmaConfig = {
+    frameworks: ["mocha", "chai", "karma-typescript", "viewport"],
+    mime: {
+      'text/x-typescript': ['ts', 'tsx']
+    },
+    client: {
+      mocha: {
+        opts: "./mocha.opts",
+      },
+    },
+    files: [
+      "./src/**/*.ts",
+      "./test/unit/**/*.ts",
+    ],
+    preprocessors: {
+      "src/**/*.ts": ["karma-typescript"],
+      "test/unit/**/*.ts": ["karma-typescript"],
+    },
+    karmaTypescriptConfig: {
+      tsconfig: "./tsconfig.test.json",
+      reports: {
+        html: {
+          "directory": "coverage",
+          "subdirectory": "./"
+        },
+        lcovonly: {
+          "directory": "coverage",
+          "filename": "lcov.info",
+          "subdirectory": "."
+        },
+      },
+      coverageOptions: {
+        instrumentation: true,
+        exclude: /test/i,
+      }
+    },
+    browsers: [],
+    customLaunchers: {
+      CustomChromeHeadless: {
+        base: "ChromeHeadless",
+        flags: ["--window-size=400,400", "--no-sandbox", "--disable-setuid-sandbox"],
+      },
+    },
+    reporters: ["mocha"],
+  };
 
-	for (let i = 1; i <= 16; ++i) {
-		videoProxies[`/videos/pano${i}.mp4`] = "/base/test/manual/videos/pano.mp4";
-	}
-	const karmaConfig = {
-		frameworks: ["mocha", "chai", "karma-typescript", "viewport"],
-		mime: {
-      		'text/x-typescript': ['ts','tsx']
-		},
-		client: {
-			mocha: {
-				opts: "./mocha.opts",
-			},
-		},
-		files: [
-			"./src/**/*.ts",
-			"./test/**/*.ts",
-			{pattern: "./test/manual/videos/**/*.*", watched: false, included: false, served: true},
-		],
-		proxies: videoProxies,
-		preprocessors: {
-			"src/**/*.ts": ["karma-typescript"],
-			"test/**/*.ts": ["karma-typescript"],
-		},
-		karmaTypescriptConfig: {
-			tsconfig: "./tsconfig.test.json",
-			reports: {
-        		html: {
-					"directory": "coverage",
-					"subdirectory": "./"
-				},
-				lcovonly: {
-					"directory": "coverage",
-					"filename": "lcov.info",
-					"subdirectory": "."
-				},
-			},
-			coverageOptions: {
-        		instrumentation: true,
-				exclude: /test/i,
-			}
-		},
-		browsers: [],
-		customLaunchers: {
-			CustomChromeHeadless: {
-				base: "ChromeHeadless",
-				flags: ["--window-size=400,400", "--no-sandbox", "--disable-setuid-sandbox"],
-			},
-		},
-		reporters: ["mocha"],
-	};
+  karmaConfig.browsers.push(config.chrome ? "Chrome" : "CustomChromeHeadless");
 
-	karmaConfig.browsers.push(config.chrome ? "Chrome" : "CustomChromeHeadless");
+  if (config.coverage) {
+    karmaConfig.reporters.push("karma-typescript");
+    karmaConfig.singleRun = true;
+  }
 
-	if (config.coverage) {
-		karmaConfig.reporters.push("karma-typescript");
-		karmaConfig.singleRun = true;
-	}
-
-	config.set(karmaConfig);
+  config.set(karmaConfig);
 };
