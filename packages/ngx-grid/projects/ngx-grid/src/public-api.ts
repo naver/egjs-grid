21bf3222fe09c032307df44d20bc7b6857f61f6c
Daybrush <daybrush@gmail.com>
2021-06-23 20:58:11 +0900

    fix: export grids

--- packages/ngx-grid/projects/ngx-grid/src/public-api.ts
+++ packages/ngx-grid/projects/ngx-grid/src/public-api.ts
@@ -8,4 +8,7 @@ export * from './lib/ngx-grid.service';
 export * from './lib/ngx-grid.component';
 export * from './lib/ngx-grid.module';
 export * from './lib/grids/ngx-masonry-grid.component';
+export * from './lib/grids/ngx-justified-grid.component';
+export * from './lib/grids/ngx-frame-grid.component';
+export * from './lib/grids/ngx-packing-grid.component';
 export * from './lib/types';
