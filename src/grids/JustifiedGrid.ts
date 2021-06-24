8e64d7e769d4c7776493e2736499cb877e9f5ae7
Daybrush <daybrush@gmail.com>
2021-06-24 11:51:05 +0900

    docs: add offset description in JustifiedGrid

--- src/grids/JustifiedGrid.ts
+++ src/grids/JustifiedGrid.ts
@@ -62,7 +62,9 @@ export interface JustifiedGridOptions extends GridOptions {
 
 /**
  * 'justified' is a printing term with the meaning that 'it fits in one row wide'. JustifiedGrid is a grid that the item is filled up on the basis of a line given a size.
+ * If 'data-grid-inline-offset' or 'data-grid-content-offset' are set for item element, the ratio is maintained except for the offset value.
  * @ko 'justified'는 '1행의 너비에 맞게 꼭 들어찬'이라는 의미를 가진 인쇄 용어다. JustifiedGrid는 용어의 의미대로 너비가 주어진 사이즈를 기준으로 아이템가 가득 차도록 배치하는 Grid다.
+ * 아이템 엘리먼트에 'data-grid-inline-offset' 또는 'data-grid-content-offset'를 설정하면 offset 값을 제외하고 비율을 유지한다.
  * @memberof Grid
  * @param {HTMLElement | string} container - A base element for a module <ko>모듈을 적용할 기준 엘리먼트</ko>
  * @param {Grid.JustifiedGrid.JustifiedGridOptions} options - The option object of the JustifiedGrid module <ko>JustifiedGrid 모듈의 옵션 객체</ko>
