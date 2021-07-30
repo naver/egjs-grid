2a977208c78bd5537b5838ab87c194c3efa4dfc6
Daybrush <daybrush@gmail.com>
2021-07-30 19:23:56 +0900

    feat: add ItemRenderer

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export * from "./grids/MasonryGrid";
 export * from "./grids/JustifiedGrid";
 export * from "./grids/FrameGrid";
 export * from "./grids/PackingGrid";
-
+export * from "./ItemRenderer";
 export * from "./types";
 export * from "./Grid";
 export * from "./GridItem";
