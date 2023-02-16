d20710c74bb3137b90b46ae910c35f33b9e7019b
Daybrush (Younkue Choi) <younkue.choi@navercorp.com>
2023-02-16 15:56:01 +0900

    fix: fix getUpdatedItems function (#85)
    * fix: fix getUpdatedItems function
    
    * test: test getUpdatedItems function

--- test/unit/utils.spec.ts
+++ test/unit/utils.spec.ts
@@ -1,3 +1,4 @@
+import { getUpdatedItems, GridItem } from "../../src";
 import { SampleGridFramework } from "./samples/SampleGridFramework";
 import { cleanup, sandbox } from "./utils/utils";
 
@@ -34,6 +35,63 @@ describe("test utils", () => {
       expect(result1).to.be.deep.equals([]);
       expect(result2).to.be.equals(grid);
       expect(sampleContainer).to.be.equals(container);
+    });
+  });
+  describe("test getUpdatedItems", () => {
+    it("should check if items are not updated when size is the same", () => {
+      // Given
+      const element = document.createElement("div");
+      const items = [
+        new GridItem(false, {
+          element,
+          orgRect: {
+            width: 100,
+            height: 100,
+            top: 0,
+            left: 0,
+          },
+          rect: {
+            width: 100,
+            height: 100,
+            top: 0,
+            left: 0,
+          },
+        }),
+      ];
+
+      // When
+      const updated = getUpdatedItems(items, [{
+        target: element,
+        size: { inlineSize: 100, blockSize: 100 },
+      }]);
+
+      // Then
+      expect(updated).to.be.lengthOf(0);
+    });
+    it("should check if item is updated when size is 0", () => {
+      // Given
+      const element = document.createElement("div");
+      const items = [
+        new GridItem(false, {
+          element,
+          orgRect: {
+            width: 100,
+            height: 100,
+            top: 0,
+            left: 0,
+          },
+        }),
+      ];
+
+      // When
+      // Even if size is the same as orgRect, the size of item's rect is 0.
+      const updated = getUpdatedItems(items, [{
+        target: element,
+        size: { inlineSize: 100, blockSize: 100 },
+      }]);
+
+      // Then
+      expect(updated[0]).to.be.equals(items[0]);
     });
   });
 });
