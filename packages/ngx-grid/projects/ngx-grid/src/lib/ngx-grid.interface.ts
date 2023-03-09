c98168aaa2294a0ce2bb8b643aee40c52d898508
arturovt <arthurandrosovich@gmail.com>
2023-03-09 23:14:26 +0200

    feat(ngx-grid): setup vanilla grid within the root zone and remove event listeners once the view is removed

--- packages/ngx-grid/projects/ngx-grid/src/lib/ngx-grid.interface.ts
+++ packages/ngx-grid/projects/ngx-grid/src/lib/ngx-grid.interface.ts
@@ -3,11 +3,15 @@
  * Copyright (c) 2021-present NAVER Corp.
  * MIT license
  */
+import { Directive } from '@angular/core';
 import VanillaGrid, { GridMethods, withGridMethods } from '@egjs/grid';
+
 import { NgxGridComponent } from './ngx-grid.component';
 
+@Directive()
 export class NgxGridInterface {
   @withGridMethods
   protected vanillaGrid!: VanillaGrid;
 }
-export interface NgxGridInterface extends GridMethods<NgxGridComponent>{}
+
+export interface NgxGridInterface extends GridMethods<NgxGridComponent> {}
